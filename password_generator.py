3437a1678297d04e3040d5ec846473c9e0c2b85c
Eric Chun <102393871+chun-eric@users.noreply.github.com>
2024-04-09 17:00:14 +0900

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,40 @@
 
+# import the random module to generate random passwords
+import random
+
+# Define the characters to be used in the password
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+# Combine all the characters
+all = lowercase + uppercase + numbers + symbols
+
+# Get length from user
+length = int(input('Enter the length of the password: '))
+
+# Get number of passwords to generate
+numberOfPasswords = int(input('Enter the number of passwords to generate: '))
+
+# generates a single password based on input length
+def generate_password(length):
+    password = ''
+    if length < 8:
+        print('Minimium length of password is 8')
+        return ''
+    else:
+        for i in range(length):
+            password += random.choice(all)
+        return password
+    
+
+# generates multiple passwords based on input length
+def generate_passwords(length, numberOfPasswords):
+    passwords = []
+    for i in range(numberOfPasswords):
+        passwords.append(generate_password(length))
+        print(passwords[i])
+
+# prints the generated passwords
+(generate_passwords(length, numberOfPasswords))
